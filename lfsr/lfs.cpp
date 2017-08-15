af581a0af779f5ef99a1ac7dd9ffa8eb9e4d8fd9
tonki <alikai812@qq.com>
2017-08-15 21:28:04 +0800

    alter lfs.cpp

---
+++ lfsr/lfs.cpp
@@ -0,0 +1,20 @@
+/************************************************
+*
+* File Name : lfs.cpp
+* Purpose :
+* Creation Date : 14-08-2017
+* Last Modified :
+* Created By :
+* Description :
+*
+************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	uint16_t a;
+
+	return 0;
+}

f2a58e3a2deca774653cba6ef4add94bfd0d7acf
tonki <alikai812@qq.com>
2017-08-15 02:37:12 +0100

    add lfsr

---
+++ lfsr/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	uint16_t start_state=0xACE1u;
+	uint16_t lfsr= start_state;
+	uint16_t bit;
+	unsigned period=0;
+	do
+	{
+		bit=((lfsr>>0)^(lfsr>>2)^(lfsr>>3)^ (lfsr >> 5))&1;
+		lfsr=(lfsr>>1) | (bit << 15);
+		++period;
+//		std::cout<<lfsr<<endl;
+	}while(lfsr!=start_state);
+	std::cout<<period<<endl;
+
+	return 0;
+}

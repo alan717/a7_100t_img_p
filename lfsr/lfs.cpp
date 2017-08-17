a30024878aae1925bb33c044336711c564c9b3bf
tonki <alikai812@qq.com>
2017-08-18 00:11:50 +0800

    lfst file

--- lfsr/lfs.cpp
+++ lfsr/lfs.cpp
@@ -1,20 +1,22 @@
-/************************************************
-*
-* File Name : lfs.cpp
-* Purpose :
-* Creation Date : 14-08-2017
-* Last Modified :
-* Created By :
-* Description :
-*
-************************************************/
 #include <iostream>
-
-using namespace std;
-
+#include <bitset>
 int main()
 {
-	uint16_t a;
+    std::bitset<5> bitRegister(1);
+    std::cout << bitRegister << std::endl;
+	int a=1;
+	while(a){
+    //get the value that will be put in the highest position after the shift
+    bool valToShiftIn = (bitRegister[0] ^ bitRegister[1]);
 
+    bitRegister >>= 1; //go ahead and shift the whole register over to the right
+
+    //mock a 1 being shifted in using bitwise-OR
+    // 1. take the result of the XOR
+    // 2. shift it up 4 to get it in the proper place
+    // 3. bitwise-OR it with the already shifted register to put the value in the proper position
+    bitRegister |= ( (valToShiftIn) << 4);
+    std::cout << bitRegister << std::endl;
+	std::cin>>a;}
 	return 0;
 }

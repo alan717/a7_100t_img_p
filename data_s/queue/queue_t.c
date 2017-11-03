edf8235b1770d17035811d30ee57ef3415a0496f
tonki <alikai812@qq.com>
2017-11-03 22:31:10 +0800

    search

--- data_s/queue/queue_t.c
+++ data_s/queue/queue_t.c
@@ -3,7 +3,7 @@
 * File Name : queue_t.cpp
 * Purpose :
 * Creation Date : 16-09-2017
-* Last Modified : Sat 16 Sep 2017 07:27:11 PM HKT
+* Last Modified : Fri 22 Sep 2017 11:58:09 PM HKT
 * Created By :
 * Description :
 *
@@ -40,7 +40,48 @@ struct Queue *createQueue()
 
 void enQueue(struct Queue *q,int k)
 {
+	struct Node *temp=newNode(k);
 
+	if(q->rear==NULL)
+	{
+		q->front=q->rear=temp;
+		return ;
+	}
+	
+	q->rear->next=temp;
+	q->rear=temp;
+}
+
+struct Node *deQueue(struct Queue *q)
+{
+	if(q->front==NULL)
+		return NULL;
+	
+	struct Node *temp=q->front;
+	q->front=q->front->next;
+	if(q->front==NULL)
+		q->rear=NULL;
+	return temp;
 
 }
+
+int main()
+{
+	struct Queue *q=createQueue();
+	enQueue(q,10);
+	enQueue(q,20);
+	deQueue(q);
+	deQueue(q);
+	enQueue(q,30);
+	enQueue(q,40);
+	enQueue(q,50);
+	struct Node *n=deQueue(q);
+	while(n!=NULL)
+	{
+		printf("item is %d\n",n->key);
+		n=deQueue(q);
+	}
+	return 0;
+}
+
 
---
+++ test/simplesearch.cpp
@@ -0,0 +1,18 @@
+/************************************************
+*
+* File Name : simplesearch.cpp
+* Purpose :
+* Creation Date : 02-10-2017
+*
+* Last Modified :
+* Created By :
+* Description :
+*
+************************************************/
+void SearchFile(fstream& fs)
+{
+	string target,line;
+	int targetlength,linelength;
+
+	cout<<"Enter the target string that you wish to search for in the list of html files:"<<endl;
+

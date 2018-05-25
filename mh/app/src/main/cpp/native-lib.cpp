0006d676135c7c7e1dbd3b6e89e0d8430d3e3f39
李云龙 <tongkai@laoyuegou.com>
2018-05-25 20:39:05 +0800

    clean code

--- mh/app/src/main/cpp/native-lib.cpp
+++ mh/app/src/main/cpp/native-lib.cpp
@@ -1,8 +1,17 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+#include <unistd.h>
 
-
+#define  TAG "fuck-mh"
+#define LOGD(...)   __android_log_print(ANDROID_LOG_DEBUG,TAG,__VA_ARGS__)
 
 void __attribute__ ((constructor)) libhook_main() {
+    LOGD("fuck mh:%s",getpid());
+
+    char path[256],buff[256];
+
+    snprintf(path, sizeof(path),"/proc/%d/smaps")
+
 
 }

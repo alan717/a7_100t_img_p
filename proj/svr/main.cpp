8810e86cf10a13c8fa7b70f0893fdd77cae78970
李云龙 <alikai812@qq.com>
2018-08-16 00:15:18 +0800

    timer async_timer modify

--- proj/svr/main.cpp
+++ proj/svr/main.cpp
@@ -1,15 +1,30 @@
 #include<iostream>
 #include<boost/asio.hpp>
-void print(const boost::system::error_code& /*e*/)
+#include<boost/bind.hpp>
+
+void print(const boost::system::error_code& e,
+	boost::asio::steady_timer* t,
+	int* count)
 {
-	std::cout << "HLW " << std::endl;
+
+	if (*count < 5)
+	{
+		std::cout << *count << std::endl;
+		++(*count);
+
+		t->expires_at(t->expiry() + boost::asio::chrono::seconds(1));
+		t->async_wait(boost::bind(print,
+			boost::asio::placeholders::error, t, count));
+	}
 }
 int main()
 {
 	boost::asio::io_context io;
 	boost::asio::steady_timer t(io, boost::asio::chrono::seconds(5));
 	//t.wait();
-	t.async_wait(&print);
+	int count = 0;
+	t.async_wait(boost::bind(print, boost::asio::placeholders::error, &t, &count));
+	
 	//std::cout << "hello wordl" << std::endl;
 	io.run();
 	return 0;

ebb13a6bed036be81eaa4c11d3e734d3cde901dd
李云龙 <alikai812@qq.com>
2018-08-16 00:50:20 +0800

    SYNC THREAD

--- proj/svr/main.cpp
+++ proj/svr/main.cpp
@@ -1,15 +1,32 @@
-#include<iostream>
-#include<boost/asio.hpp>
-#include<boost/bind.hpp>
+//
+// timer.cpp
+// ~~~~~~~~~
+//
+// Copyright (c) 2003-2018 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind.hpp>
 
 class printer
 {
 public:
 	printer(boost::asio::io_context& io)
-		: timer_(io, boost::asio::chrono::seconds(1)),
+		: strand_(io),
+		timer1_(io, boost::asio::chrono::seconds(1)),
+		timer2_(io, boost::asio::chrono::seconds(1)),
 		count_(0)
 	{
-		timer_.async_wait(boost::bind(&printer::print, this));
+		timer1_.async_wait(boost::asio::bind_executor(strand_,
+			boost::bind(&printer::print1, this)));
+
+		timer2_.async_wait(boost::asio::bind_executor(strand_,
+			boost::bind(&printer::print2, this)));
 	}
 
 	~printer()
@@ -17,20 +34,38 @@ public:
 		std::cout << "Final count is " << count_ << std::endl;
 	}
 
-	void print()
+	void print1()
 	{
-		if (count_ < 5)
+		if (count_ < 10)
 		{
-			std::cout << count_ << std::endl;
+			std::cout << "Timer 1: " << count_ << std::endl;
 			++count_;
 
-			timer_.expires_at(timer_.expiry() + boost::asio::chrono::seconds(1));
-			timer_.async_wait(boost::bind(&printer::print, this));
+			timer1_.expires_at(timer1_.expiry() + boost::asio::chrono::seconds(1));
+
+			timer1_.async_wait(boost::asio::bind_executor(strand_,
+				boost::bind(&printer::print1, this)));
+		}
+	}
+
+	void print2()
+	{
+		if (count_ < 10)
+		{
+			std::cout << "Timer 2: " << count_ << std::endl;
+			++count_;
+
+			timer2_.expires_at(timer2_.expiry() + boost::asio::chrono::seconds(1));
+
+			timer2_.async_wait(boost::asio::bind_executor(strand_,
+				boost::bind(&printer::print2, this)));
 		}
 	}
 
 private:
-	boost::asio::steady_timer timer_;
+	boost::asio::io_context::strand strand_;
+	boost::asio::steady_timer timer1_;
+	boost::asio::steady_timer timer2_;
 	int count_;
 };
 
@@ -38,7 +73,9 @@ int main()
 {
 	boost::asio::io_context io;
 	printer p(io);
+	boost::thread t(boost::bind(&boost::asio::io_context::run, &io));
 	io.run();
+	t.join();
 
 	return 0;
 }

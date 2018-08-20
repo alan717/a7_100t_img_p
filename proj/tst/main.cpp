f46325c29331da46dcfc81b0bb2c88d9dea70628
辛多雷的荣耀 <alikai812@qq.com>
2018-08-20 14:20:32 +0800

    bfs

---
+++ proj/tst/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	// A character array in C/C++/Java
+	char arr1[] = { 'g', 'e', 'e', 'k', 's' };
+
+	// An Integer array in C/C++/Java
+	int arr2[] = { 10, 20, 30, 40, 50 };
+
+	// Item at i'th index in array is typically accessed
+	// as "arr[i]".  For example arr1[0] gives us 'g'
+	// and arr2[3] gives us 40.
+	std::cout << "hello world " << std::endl;
+	return 0;
+}

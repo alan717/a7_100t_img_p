a23a757e7cd1a662d074039602f5e4b19e21e43b
alan717 <alikai812@qq.com>
2018-09-06 12:08:05 +0800

    update init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,9 +6,10 @@
          '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'load-path "~/.emacs.d/my/")
 
-(require 'powerline)
+;;
 ;;;(require 'smex)
-
+(require 'flycheck-rtags)
+(
 (package-initialize)
 
 (when (not package-archive-contents)
@@ -44,6 +45,7 @@
 
 (package-install-selected-packages)
 ;; config company
+
 (req-package company
 	     :config
 	     (progn
@@ -103,18 +105,78 @@
 (req-package rtags
   :config
   (progn
+
+    ;;funclis
+    (defun use-rtags (&optional useFileManager)
+  (and (rtags-executable-find "rc")
+       (cond ((not (gtags-get-rootpath)) t)
+             ((and (not (eq major-mode 'c++-mode))
+                   (not (eq major-mode 'c-mode))) (rtags-has-filemanager))
+             (useFileManager (rtags-has-filemanager))
+             (t (rtags-is-indexed)))))
+
+(defun tags-find-symbol-at-point (&optional prefix)
+  (interactive "P")
+  (if (and (not (rtags-find-symbol-at-point prefix)) rtags-last-request-not-indexed)
+      (gtags-find-tag)))
+(defun tags-find-references-at-point (&optional prefix)
+  (interactive "P")
+  (if (and (not (rtags-find-references-at-point prefix)) rtags-last-request-not-indexed)
+      (gtags-find-rtag)))
+(defun tags-find-symbol ()
+  (interactive)
+  (call-interactively (if (use-rtags) 'rtags-find-symbol 'gtags-find-symbol)))
+(defun tags-find-references ()
+  (interactive)
+  (call-interactively (if (use-rtags) 'rtags-find-references 'gtags-find-rtag)))
+(defun tags-find-file ()
+  (interactive)
+  (call-interactively (if (use-rtags t) 'rtags-find-file 'gtags-find-file)))
+(defun tags-imenu ()
+  (interactive)
+  (call-interactively (if (use-rtags t) 'rtags-imenu 'idomenu)))
+
+    (define-key c-mode-base-map (kbd "M-.") (function tags-find-symbol-at-point))
+
+    (define-key c-mode-base-map (kbd "M-,") (function tags-find-references-at-point))
+
+    (define-key c-mode-base-map (kbd "M-;") (function tags-find-file))
+
+    (define-key c-mode-base-map (kbd "C-.") (function tags-find-symbol))
+
+    (define-key c-mode-base-map (kbd "C-,") (function tags-find-references))
+
+    (define-key c-mode-base-map (kbd "C-<") (function rtags-find-virtuals-at-point))
+
+    (define-key c-mode-base-map (kbd "M-i") (function tags-imenu))
+
+
+    (define-key global-map (kbd "M-.") (function tags-find-symbol-at-point))
+
+    (define-key global-map (kbd "M-,") (function tags-find-references-at-point))
+
+    (define-key global-map (kbd "M-;") (function tags-find-file))
+
+    (define-key global-map (kbd "C-.") (function tags-find-symbol))
+
+    (define-key global-map (kbd "C-,") (function tags-find-references))
+
+    (define-key global-map (kbd "C-<") (function rtags-find-virtuals-at-point))
+
+    (define-key global-map (kbd "M-i") (function tags-imenu))
+
+    (define-key c-mode-base-map (kbd "<C-tab>") (function company-complete))
+;    (define-key c-mode-base-map
+
     (unless (rtags-executable-find "rc") (error "Binary rc is not installed!"))
     (unless (rtags-executable-find "rdm") (error "Binary rdm is not installed!"))
-    (setq rtags-path '/home/ququ/.emacs.d/rtags/bin)
+    (setq rtags-path "/home/ququ/.emacs.d/rtags/bin")
+
     (add-hook 'c-mode-hook 'rtags-start-process-unless-running)
     (add-hook 'c++-mode 'rtags-start-process-unless-running)
-    
-    
-    (define-key c-mode-base-map (kbd "M-.") 'rtags-find-symbol-at-point)
-    (define-key c-mode-base-map (kbd "M-,") 'rtags-find-references-at-point)
-    (define-key c-mode-base-map (kbd "M-?") 'rtags-display-summary)
-;    (define-key c-mode-base-map
-    
+    (setq rtags-autostart-diagnostics t)
+    (setq rtags-completions-enabled t)
+    (rtags-diagnostics)
     (rtags-enable-standard-keybindings)
     (rtags-restart-process)
     

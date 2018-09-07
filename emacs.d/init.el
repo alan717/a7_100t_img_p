2b591c8741ef23e67b53381cf32c781f0f517714
李云龙 <alikai812@qq.com>
2018-09-07 14:06:44 +0800

    update gnu melpa

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,7 +3,9 @@
 (require 'package)
 ;;;code
 (add-to-list 'package-archives
-         '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages"))
 (add-to-list 'load-path "~/.emacs.d/my/")
 
 ;;

14bb8102d94284d959ac551a307eae04195f50d5
DeathKnight <tongkai@laoyuegou.com>
2018-12-07 00:33:14 +0800

    go mode truck

---
+++ emacs.d/go_env/init.el
@@ -0,0 +1,39 @@
+(setq inhibit-startup-message t)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(setq url-proxy-services '(("no_proxy" . "baidu.com")
+                           ("http" . "192.168.0.106:8118")))
+(package-initialize)
+(when (not package-archive-contents)
+    (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+ '(package-selected-packages
+   (quote
+    ( window-numbering nyan-mode solarized-theme  projectile cmake-mode  use-package undo-tree counsel-projectile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(package-install-selected-packages)
+;;(require 'req-package)
+
+
+
+;; config company
+(load-theme 'solarized-dark)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))))
